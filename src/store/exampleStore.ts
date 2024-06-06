2c4586c31251df6a42904a778da7ad9fabe32c5e
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-06-06 05:42:19 -0600

    map fn

--- src/store/exampleStore.ts
+++ src/store/exampleStore.ts
@@ -1,3 +1,12 @@
-import {atom} from 'nanostores';
+import {atom, map} from 'nanostores';
 
 export const database=atom(false);
+
+export type CartItem = {
+    id: string;
+    name: string;
+    imageSrc: string;
+    quantity: number;
+  }
+  
+  export const cartItems = map<Record<string, CartItem>>({});
